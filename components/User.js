5e4d438f2e9f19e003f6c8ed8abee87380e7a5a2
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-25 22:11:44 +0100

    add scoped style component

--- components/User.js
+++ components/User.js
@@ -4,6 +4,17 @@ const User = (props) => (
     <div>
         <p>Hi my name is {props.name}</p>
         <p>I am {props.age} old</p>
+        <style jsx>{`
+        div {
+        border: 1px solid #eee;
+        box-shadow: 0 2px 3px #ccc;
+        padding: 20px;
+        text-align: center;
+        }    
+         p {
+            color: red
+            }`
+        }</style>
     </div>);
 
 export default User;
