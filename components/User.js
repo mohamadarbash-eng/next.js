f2a25affe2902b4db187d1fe9119e092cb4f4090
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-25 21:44:42 +0100

    add based class component

---
+++ components/User.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const User = (props) => (
+    <div>
+        <p>Hi my name is {props.name}</p>
+        <p>I am {props.age} old</p>
+    </div>);
+
+export default User;
