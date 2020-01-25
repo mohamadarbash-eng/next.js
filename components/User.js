34fff8efc2033b484ceea42123c9e71ced7ab327
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-25 22:29:21 +0100

    add swr and unfetch packages fetch data

--- components/User.js
+++ components/User.js
@@ -1,10 +1,25 @@
 import React from "react";
+import fetch from 'unfetch'
+import useSWR from 'swr'
 
-const User = (props) => (
-    <div>
-        <p>Hi my name is {props.name}</p>
-        <p>I am {props.age} old</p>
-        <style jsx>{`
+const API_URL = 'https://api.github.com';
+async function fetcher(path) {
+    const res = await fetch(API_URL + path);
+    const json = await res.json();
+    return json
+}
+
+const User = (props) => {
+    const { data, error } = useSWR('/repos/zeit/next.js', fetcher);
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
+    return( <div>
+            <p>Hi my name is {props.name}</p>
+            <p>I am {props.age} old</p>
+        <div>Next stars: {data.stargazers_count}</div>
+            <style jsx>{`
         div {
         border: 1px solid #eee;
         box-shadow: 0 2px 3px #ccc;
@@ -14,7 +29,10 @@ const User = (props) => (
          p {
             color: red
             }`
-        }</style>
-    </div>);
+            }</style>
+        </div>)
+
+}
+;
 
 export default User;
